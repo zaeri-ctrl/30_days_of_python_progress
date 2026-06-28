214fec818e4307c5d73593e0dd3ca090e3f96cd9
zaeri <hopesnaddreams111@gmail.com>
2026-06-28 18:27:55 -0500

    Add files via upload

---
+++ day_01.py
@@ -0,0 +1,15 @@
+#Open the python interactive shell and do the following operations. The operands are 3 and 4.
+#Addition:
+print(3 + 4)  #7
+#Substraction:
+print(3 - 4)  #-1
+#Multiplication:
+print(3 * 4)   #12
+#Modulus:
+print(3 % 4)   #3
+#Division:
+print(3 / 4 )  #0.75
+#Exponential:
+print(3 ** 4)   #81
+#Floor division operation:
+print(3 // 4)   #0
